63df769be1b3c14f2d1a9bdf7d6534641653cdef
GitEma <68866131+GitEma@users.noreply.github.com>
2020-08-12 20:37:10 -0300

    Update!
    El nuevo index es la página de LogIn. El anterior index.html ahora es home.html, todos los link <a> han sido actualizados acorde a este cambio. Se realizó una petición para traer el .json correspondiente para mostrarlo en la sección productos; para todo esto se han modificado el nuevo index.html, el products.js y los styles.css

--- js/products.js
+++ js/products.js
@@ -1,6 +1,92 @@
+const ORDER_ASC_BY_NAME = "AZ";
+const ORDER_DESC_BY_NAME = "ZA";
+const SOLD_COUNT = "soldCount.";
+var currentCategoriesArray = [];
+var currentSortCriteria = undefined;
+var minCount = undefined;
+var maxCount = undefined;
+
+function sortCategories(criteria, array) {
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME) {
+        result = array.sort(function (a, b) {
+            if (a.name < b.name) { return -1; }
+            if (a.name > b.name) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_DESC_BY_NAME) {
+        result = array.sort(function (a, b) {
+            if (a.name > b.name) { return -1; }
+            if (a.name < b.name) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_BY_PROD_COUNT) {
+        result = array.sort(function (a, b) {
+            let aCount = parseInt(a.productCount);
+            let bCount = parseInt(b.productCount);
+
+            if (aCount > bCount) { return -1; }
+            if (aCount < bCount) { return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
+function showCategoriesList() {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < currentCategoriesArray.length; i++) {
+        let category = currentCategoriesArray[i];
+
+        htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ category.name + `</h4>
+                            <small class="text-muted">` + category.soldCount + ` artículos vendidos.</small>
+                        </div>
+                        <p class="mb-1">` + category.description + `</p>
+                        <div class="precio">
+                        <p>` + category.cost + ' ' + category.currency + `</p>
+                        <button id="buyBtn">Comprar.</button>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            `
+    }
+
+    document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+}
+
+
+function sortAndShowCategories(sortCriteria, categoriesArray) {
+    currentSortCriteria = sortCriteria;
+
+    if (categoriesArray != undefined) {
+        currentCategoriesArray = categoriesArray;
+    }
+
+    currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
+
+    //Muestro las categorías ordenadas
+    showCategoriesList();
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+        }
 
+    });
 });
